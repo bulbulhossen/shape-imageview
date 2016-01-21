de2a3eed19dabbbe77baad2295646f793b0b18ea
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-01-21 14:22:59 +0600

    RippleView update : Image , Textview ,Editbox etc

--- app/src/main/java/com/bulbulhossen/convenientbanner/MainActivity.java
+++ app/src/main/java/com/bulbulhossen/convenientbanner/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bulbulhossen.convenientbanner;
 
+import android.graphics.drawable.RippleDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.andexert.library.RippleView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RippleView rippleView  = (RippleView) findViewById(R.id.more);
+
+        rippleView.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+
+            @Override
+            public void onComplete(RippleView rippleView) {
+                Log.d("Sample", "Ripple completed");
+            }
+
+        });
+
     }
 }
